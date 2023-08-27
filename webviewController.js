7bc6f3b883747c6d32185cf92ab9752ab7be69ed
xkwxdyy <kangweixia_xdyy@163.com>
2023-08-27 22:56:56 +0800

    调整间距

--- webviewController.js
+++ webviewController.js
@@ -89,123 +89,102 @@ var mnPinCopyController = JSB.defineClass(
     },
     viewWillDisappear: function(animated) {
     },
-  viewWillLayoutSubviews: function() {
-    if (self.miniMode) {
-      return
-    }
-    // // viewFrame 获取当前窗口的边界对象
-    // var viewFrame = self.view.bounds;
-    // // xLeft：窗口左边界
-    // var xLeft     = viewFrame.x
-    // // xRight：窗口右边界
-    // var xRight    = xLeft + viewFrame.width
-    // // yTop：窗口上边界
-    // var yTop      = viewFrame.y
-    // // yBottom：窗口下边界
-    // var yBottom   = yTop + viewFrame.height
-    // var halfHeight = (viewFrame.height-100)*0.5
-    // var halfWidth = (viewFrame.width-15)*0.5
-    // viewFrame.y = 5
-    // viewFrame.x = 5
-    // viewFrame.height -= 10
-    // viewFrame.width -= 10
-    // // 设置输入框的边界
-    // self.textviewPinText.frame = viewFrame
-
-    // self.closeButton.frame = {x: xRight-35,y: yBottom-35, width: 30, height: 30};
-    // self.moveButton.frame = {x: xLeft+5, y: yBottom-35, width: xRight-10, height: 30};
-    // self.copyButton.frame = { x: viewFrame.width-60,  y: yBottom-80,  width: 60,  height: 30};
-    // self.pasteButton.frame = { x: viewFrame.width-130,  y: yBottom-80,  width: 60,  height: 30};
-
-    // viewFrame.y = 15 + halfHeight
-    // viewFrame.height = 30
-    // viewFrame.width = halfWidth
-    var viewFrame = self.view.bounds;
-    var xLeft     = viewFrame.x
-    var xRight    = xLeft + viewFrame.width
-    var yTop      = viewFrame.y
-    var yBottom   = yTop + viewFrame.height
-    self.closeButton.frame = {x: xRight-35,y: yBottom-35,width: 30,height: 30};
-    self.moveButton.frame = {x: xLeft+5 ,y: yBottom-35,width: xRight-10,height: 30};
-    var halfHeight = (viewFrame.height-100)*0.5
-    var halfWidth = (viewFrame.width-15)*0.5
-    viewFrame.y = 5
-    viewFrame.x = 5
-    viewFrame.height = halfHeight
-    viewFrame.width = viewFrame.width -10
-
-    self.textviewPinText.frame = viewFrame
-
-    self.pasteButton.frame = {  x: viewFrame.width-60,  y: viewFrame.height-30,  width: 60,  height: 30,};
-    viewFrame.y = 10+halfHeight
-
-    self.copyButton.frame = {  x: viewFrame.width-60,  y: halfHeight+viewFrame.height-25,  width: 60,  height: 30,};
-    viewFrame.y = 15+halfHeight*2
-    viewFrame.height = 45
-    viewFrame.width = halfWidth
-  },
-  scrollViewDidScroll: function() {
-  },
-  pasteButtonTapped: function() {
-    // 将剪切板的内容输出到 input 框
-    self.textviewPinText.text = UIPasteboard.generalPasteboard().string
-  },
-  copyButtonTapped: function() {
-    // 将 output 框的内容复制到剪切板
-    UIPasteboard.generalPasteboard().string = self.textviewPinText.text
-  },
-  closeButtonTapped: function() {
-    // 隐藏窗口
-    self.view.hidden = true;
-  },
-  onMoveGesture:function (gesture) {
-    let locationToMN = gesture.locationInView(self.appInstance.studyController(self.view.window).view)
-    if ( (Date.now() - self.moveDate) > 100) {
-      let translation = gesture.translationInView(self.appInstance.studyController(self.view.window).view)
-      let locationToBrowser = gesture.locationInView(self.view)
-      let locationToButton = gesture.locationInView(gesture.view)
-      let buttonFrame = self.moveButton.frame
-      let newY = locationToButton.y-translation.y 
-      let newX = locationToButton.x-translation.x
-      if (gesture.state !== 3 && (newY<buttonFrame.height && newY>0 && newX<buttonFrame.width && newX>0 && Math.abs(translation.y)<20 && Math.abs(translation.x)<20)) {
-        self.locationToBrowser = {x:locationToBrowser.x-translation.x,y:locationToBrowser.y-translation.y}
+    viewWillLayoutSubviews: function() {
+      // 检查是否在 miniMode，如果是则直接返回
+      if (self.miniMode) {
+        return;
       }
-    }
-    self.moveDate = Date.now()
-    let location = {x:locationToMN.x - self.locationToBrowser.x,y:locationToMN.y -self.locationToBrowser.y}
-    let frame = self.view.frame
-    var viewFrame = self.view.bounds;
-    let studyFrame = self.appInstance.studyController(self.view.window).view.bounds
-    let y = location.y
-    if (y<=0) {
-      y = 0
-    }
-    if (y>=studyFrame.height-15) {
-      y = studyFrame.height-15
-    }
-    let x = location.x
-    self.view.frame = {x:x,y:y,width:frame.width,height:frame.height}
-    self.currentFrame  = self.view.frame
-  },
-  onResizeGesture:function (gesture) {
-    self.custom = false;
-    self.dynamic = false;
-    let baseframe = gesture.view.frame
-    let locationInView = gesture.locationInView(gesture.view)
-    let frame = self.view.frame
-    let width = locationInView.x+baseframe.x+baseframe.width*0.5
-    let height = locationInView.y+baseframe.y+baseframe.height*0.5
-    // 最小宽度
-    if (width <= 200) {
-      width = 200
-    }
-    // 最小高度
-    if (height <= 100) {
-      height = 100
-    }
-    self.view.frame = {x:frame.x,y:frame.y,width:width,height:height}
-    self.currentFrame  = self.view.frame
-  },
+      
+      // 获取当前视图的边界
+      var viewFrame = self.view.bounds;
+      // 计算左、右、上、下边界
+      var xLeft     = viewFrame.x;
+      var xRight    = xLeft + viewFrame.width;
+      var yTop      = viewFrame.y;
+      var yBottom   = yTop + viewFrame.height;
+    
+      // 下面的代码用于填满整个区域
+      // 调整视图的 x 和 y 坐标（相当于给视图加了一个边距）
+      viewFrame.y = 5;
+      viewFrame.x = 5;
+      // 调整视图的高度和宽度
+      viewFrame.height -= 10;  // 根据需要调整这个值
+      viewFrame.width -= 10;   // 根据需要调整这个值
+      
+      // 现在 textviewPinText 应该填满整个区域
+      self.textviewPinText.frame = viewFrame;
+      
+      // 设置关闭按钮的位置和尺寸
+      self.closeButton.frame = {x: xRight-35, y: yBottom-35, width: 30, height: 30};
+      // 设置移动按钮的位置和尺寸
+      self.moveButton.frame = {x: xLeft+5, y: yBottom-35, width: xRight-10, height: 30};
+      // 设置复制按钮的位置和尺寸
+      self.copyButton.frame = {x: viewFrame.width-130, y: viewFrame.height-70, width: 60, height: 30};
+      // 设置粘贴按钮的位置和尺寸
+      self.pasteButton.frame = {x: viewFrame.width-60, y: viewFrame.height-70, width: 60, height: 30};
+    },
+    scrollViewDidScroll: function() {
+    },
+    pasteButtonTapped: function() {
+      // 将剪切板的内容输出到 input 框
+      self.textviewPinText.text = UIPasteboard.generalPasteboard().string
+    },
+    copyButtonTapped: function() {
+      // 将 output 框的内容复制到剪切板
+      UIPasteboard.generalPasteboard().string = self.textviewPinText.text
+    },
+    closeButtonTapped: function() {
+      // 隐藏窗口
+      self.view.hidden = true;
+    },
+    onMoveGesture:function (gesture) {
+      let locationToMN = gesture.locationInView(self.appInstance.studyController(self.view.window).view)
+      if ( (Date.now() - self.moveDate) > 100) {
+        let translation = gesture.translationInView(self.appInstance.studyController(self.view.window).view)
+        let locationToBrowser = gesture.locationInView(self.view)
+        let locationToButton = gesture.locationInView(gesture.view)
+        let buttonFrame = self.moveButton.frame
+        let newY = locationToButton.y-translation.y 
+        let newX = locationToButton.x-translation.x
+        if (gesture.state !== 3 && (newY<buttonFrame.height && newY>0 && newX<buttonFrame.width && newX>0 && Math.abs(translation.y)<20 && Math.abs(translation.x)<20)) {
+          self.locationToBrowser = {x:locationToBrowser.x-translation.x,y:locationToBrowser.y-translation.y}
+        }
+      }
+      self.moveDate = Date.now()
+      let location = {x:locationToMN.x - self.locationToBrowser.x,y:locationToMN.y -self.locationToBrowser.y}
+      let frame = self.view.frame
+      var viewFrame = self.view.bounds;
+      let studyFrame = self.appInstance.studyController(self.view.window).view.bounds
+      let y = location.y
+      if (y<=0) {
+        y = 0
+      }
+      if (y>=studyFrame.height-15) {
+        y = studyFrame.height-15
+      }
+      let x = location.x
+      self.view.frame = {x:x,y:y,width:frame.width,height:frame.height}
+      self.currentFrame  = self.view.frame
+    },
+    onResizeGesture:function (gesture) {
+      self.custom = false;
+      self.dynamic = false;
+      let baseframe = gesture.view.frame
+      let locationInView = gesture.locationInView(gesture.view)
+      let frame = self.view.frame
+      let width = locationInView.x+baseframe.x+baseframe.width*0.5
+      let height = locationInView.y+baseframe.y+baseframe.height*0.5
+      // 最小宽度
+      if (width <= 200) {
+        width = 200
+      }
+      // 最小高度
+      if (height <= 100) {
+        height = 100
+      }
+      self.view.frame = {x:frame.x,y:frame.y,width:width,height:height}
+      self.currentFrame  = self.view.frame
+    },
 });
 
 mnPinCopyController.prototype.setButtonLayout = function (button, targetAction) {
